6a605127c2fd0447ad74f765dfc0f60f2d0e4d9d
josiah <josiahdamiwilliams@gmail.com>
2020-07-22 16:06:47 +0100

    Day 1 & 2

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,47 +2,65 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
     LDI = 0b10000010
     HLT = 0b00000001
     PRN = 0b01000111
+    MUL = 0b10100010
 
     def __init__(self):
         """Construct a new CPU."""
-        self.ram = [0] * 256
         self.reg = [0] * 8
+        self.ram = [0] * 256
         self.pc = 0
-        self.running = True
+        self.running = False
 
-    def load(self):
+    def ram_write(self, mdr, mar):
+        self.ram[mar] = mdr
+
+    def ram_read(self, mar):
+        return self.ram[mar]
+
+    def load(self, filename):
         """Load a program into memory."""
 
-        address = 0
+        # for now, we've just hardcoded a program
+        try:
+            address = 0
 
-        # For now, we've just hardcoded a program:
+            with open(filename) as f:
+                for line in f:
+                    # split before comment
+                    comment_split = line.split('#')
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+                    # convert to a number splitting and stripping
+                    num = comment_split[0]. strip()
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+                    if num != "":
+                        continue  # ignore blank spaces
 
+                    val = int(num, 2)
+
+                    # store val in memory at the address
+                    self.ram[address] = val
+
+                    address += 1
+
+        except FileNotFoundError:
+            print(f"{sys.argv[0]}: {filename} not found")
+            sys.exit(2)
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -54,8 +72,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -68,28 +86,29 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
-        while self.running:
-            ir = self.ram_read(self.pc)
-            if ir == self.LDI:
-                reg_num = self.ram_read(self.pc + 1)
-                value = self.ram_read(self.pc + 2)
-                self.ram_write(reg_num, value)
-                self.pc += 3
-            elif ir == self.HLT:
-                self.running = False
+        while not self.running:
+            ir = self.ram[self.pc]
+            instruction_length = ((ir >> 6) & 0b11) + \
+                1  # (bitshifted instruction)
+            reg_num = self.ram_read(self.pc + 1)
+            value = self.ram_read(self.pc + 2)
+            # set the instruction length here (extract)
+
+            # halt
+            if ir == self.HLT:
+                self.running = True
+
+            # LDI
+            elif ir == self.LDI:
+                self.reg[reg_num] = value
+
+            # PRN
             elif ir == self.PRN:
-                reg_num = self.ram[self.pc + 1]
-                print(self.ram[reg_num])
-                self.pc += 2
-            else:
-                print(f'Unknown instruction {ir} at address {self.pc}')
-                sys.exit(1)
+                print(self.reg[reg_num])
 
+            elif ir == self.MUL:
+                self.alu("MUL", reg_num, value)
 
+            self.pc += instruction_length
 
-    def ram_read(self, address):
-        return self.ram[address]
-
-    def ram_write(self, address, value):
-        self.ram[address] = value
+   
